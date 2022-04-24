58cbf623c5ce31148001160c5716b5bea2130b79
Matheus Kyoshi Shoji <63069352+MKShoji@users.noreply.github.com>
2022-04-24 19:32:35 -0300

    Alteração do gráfico de Pizza
    Foi alterado as informações do gráfico com os dados transferidos do banco de dados

--- Aula1_PW3/index.php
+++ Aula1_PW3/index.php
@@ -29,53 +29,35 @@
 <section>
 
 <div class="container">
+	
 	<?php 
-		$mb = 10;
-		$b = 5;
-		$r = 1;
-		$i = 4;
-	?>
-
-	<?php
-		$stmt = $pdo->prepare("select count(*) from tbProduto");	
-		$stmt ->execute();			
+		$stmt = $pdo->prepare("select count(*) from tbProduto where idCategoria = 1;");
+		$stmt ->execute();
 		$row = $stmt ->fetch(PDO::FETCH_NUM);
-		echo "Total de produtos: $row[0]";			
 	?>
 
-	<?php
-		$stmt2 = $pdo->prepare("select sum(valor) from tbProduto");	
-		$stmt2 ->execute();			
+<?php 
+		$stmt2 = $pdo->prepare("select count(*) from tbProduto where idCategoria = 2;");
+		$stmt2 ->execute();
 		$row2 = $stmt2 ->fetch(PDO::FETCH_NUM);
-		echo "Soma dos valores dos produtos: $row2[0]";			
 	?>
 
-	<?php
-		$stmt3 = $pdo->prepare("select avg(valor) from tbProduto");	
-		$stmt3 ->execute();			
+<?php 
+		$stmt3 = $pdo->prepare("select count(*) from tbProduto where idCategoria = 3;");
+		$stmt3 ->execute();
 		$row3 = $stmt3 ->fetch(PDO::FETCH_NUM);
-		echo "Média dos valores dos produtos: $row3[0]";			
-	?> 
+	?>
 
-	<?php
-		$stmt4 = $pdo->prepare("select max(valor) from tbProduto");	
-		$stmt4 ->execute();			
+<?php 
+		$stmt4 = $pdo->prepare("select count(*) from tbProduto where idCategoria = 4;");
+		$stmt4 ->execute();
 		$row4 = $stmt4 ->fetch(PDO::FETCH_NUM);
-		echo "Valor do produto mais caro: $row4[0]";			
-	?> 
+	?>
 
-	<?php
-		$stmt5 = $pdo->prepare("select min(valor) from tbProduto");	
-		$stmt5 ->execute();			
+<?php 
+		$stmt5 = $pdo->prepare("select count(*) from tbProduto where idCategoria = 5;");
+		$stmt5 ->execute();
 		$row5 = $stmt5 ->fetch(PDO::FETCH_NUM);
-		echo "Valor do produto mais barato: $row5[0]";			
-	?>   
-
-	<?php 
-		$stmt6 = $pdo->prepare("select count(*) from tbProduto where idCategoria = 1;");
-		$stmt6 ->execute();
-		$row6 = $stmt6 ->fetch(PDO::FETCH_NUM);
-		echo "Quantidade de produtos de alimentação: $row6[0]";
 	?>
 
 	<!-- https://github.com/d3/d3/wiki/Gallery -->
@@ -101,17 +83,17 @@
 		data.addColumn('string', 'Topping');
 		data.addColumn('number', 'Slices');
 		data.addRows([
-			['Alimentação',<?php echo $row6[0]; ?> ],          
-			['Tecnologia',<?php echo $b; ?>],          
-			['Vestimenta',<?php echo $r; ?>],          
-			['Higiene',<?php echo $i; ?>],
-			['Moveis',<?php echo $i; ?>],           
+			['Alimentação',<?php echo $row[0]; ?> ],          
+			['Tecnologia',<?php echo $row2[0]; ?>],          
+			['Vestimenta',<?php echo $row3[0]; ?>],          
+			['Higiene',<?php echo $row4[0]; ?>],
+			['Moveis',<?php echo $row5[0]; ?>],           
 		]);
 
 		// Set chart options
 		var options = {'title':'Quantidade de produtos por categoria',
-						'width':800,
-						'height':600};
+						'width':500,
+						'height':300};
 
 		// Instantiate and draw our chart, passing in some options.
 		var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
